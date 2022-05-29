a85cda93b217dd96e6cfea3573118441270aeae8
gustavocuore <gucuore@gmail.com>
2022-05-28 22:05:14 -0300

    deixando o código mais funcional com 'for'

--- main.js
+++ main.js
@@ -6,19 +6,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-
-    contador++;
-
-    //console.log(contador);
 }
